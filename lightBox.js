f6e74c689f3edffeebe3f0c3165ad770c97768e3
ajmhyd <ajmhyd@mail.missouri.edu>
2017-07-26 23:56:24 -0500

    Working lightbox

--- lightBox.js
+++ lightBox.js
@@ -1,47 +1,41 @@
-alert("Hello! I am an alert box1!!");
-/*global $ */
-$(document).ready(function () {
-    
-    "use strict";
-    alert("Hello! I am an alert box2!!");
-    function openModal() {
-        document.getElementById('myModal').style.display = "block";
-    }
+"use strict";
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
 
-    function closeModal() {
-        document.getElementById('myModal').style.display = "none";
-    }
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
 
-    var slideIndex = 1;
-    showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("demo");
-        var captionText = document.getElementById("caption");
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-        captionText.innerHTML = dots[slideIndex - 1].alt;
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        slideIndex = 1;
     }
-});
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
